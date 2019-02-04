fc8f8a8ecca1bbfa8219c4c1f576bd90ddf8baae
Mark Garner <mgarner@microsoft.com>
2019-02-04 13:11:16 -0600

    added .net console app to generate vscode snippet file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ARMSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
