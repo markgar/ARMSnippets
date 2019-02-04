752346e861d549edbb88312c9118f15d6558812b
Mark Garner <mgarner@microsoft.com>
2019-02-04 16:09:44 -0600

    fixed some stuff, added a lot of templates

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using ARMSnippets.models;
+using Newtonsoft.Json;
 
 namespace ARMSnippets
 {
@@ -6,7 +11,20 @@ namespace ARMSnippets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Hashtable snippets = new Hashtable();
+
+            foreach (string file in Directory.GetFiles("templates"))
+            {
+                string fileName = file.Split("\\")[1];
+                string fileNameWithoutExtension = fileName.Split(".")[0];
+                snippets.Add(fileNameWithoutExtension, new Snippet(fileName));
+            }
+
+            string json = JsonConvert.SerializeObject(snippets);
+            System.Console.WriteLine(json);
+
+            File.WriteAllText("snippets.json", json);
         }
     }
 }
---
+++ models/Snippet.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace ARMSnippets.models
+{
+    public class Snippet
+    {
+        public string prefix {get; set;}
+        public List<string> body {get; set;}
+        public string description {get; set;}
+
+        public Snippet(string Filename)
+        {
+            string line;
+            body = new List<string>();
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@".\templates\" + Filename);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string lineTransformed = string.Empty;
+
+                body.Add(line);
+            }
+
+            file.Close();
+
+            prefix = "arm-" + Filename.Split('.')[0];
+            description = Filename.Split('.')[0] + " ARM Template";
+        }
+    }
+}
